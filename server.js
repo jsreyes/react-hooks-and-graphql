a4832d4f5110f120dd0a8c67d6ada8f86a32effd
Javier Sebastián Reyes Mogollón <jreyes@belatrixsf.com>
2019-12-23 11:54:03 -0500

    Connect with cluster

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers.js');
 
+// Use mongoose to conect the cluster
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+// Method to connect cluster
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true}) // remove deprecation warning when connect from Mongoose
+        .then(() => console.log('DB connected!'))
+        .catch( err => console.error(err))
+
 const server = new ApolloServer ({
     typeDefs,
     resolvers
